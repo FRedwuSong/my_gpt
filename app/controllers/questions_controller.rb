b438de13690847984fcf7726fed41f49bb4e1f0c
FRedwuSong <diadora1166@gmail.com>
2023-07-17 19:38:44 +0800

    feat:: Build a simple chat with Hotwire
    Currently,  the create  action only reponse "I don't know."

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def index; end
+
+  def create
+    @answer = "I don't know."
+  end
+
+  private
+
+  def question
+    params[:question][:question]
+  end
+end
