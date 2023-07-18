c56566ba9afa61bef2a0c81ea89a25350d083156
FRedwuSong <diadora1166@gmail.com>
2023-07-18 14:40:08 +0800

    feat:: Create table items
    enable embedding use gem, neighbors

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_132033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "vector"
+
+  create_table "items", force: :cascade do |t|
+    t.string "page_name"
+    t.text "text"
+    t.vector "embedding", limit: 1536
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
